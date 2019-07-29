eb27cd483ecf9bce12edc7614a5091d0916319c0
eyakub <eyakubsorkar@gmail.com>
2019-07-29 14:16:41 +0600

    #60 accessing the dom with refs

--- Deep dive the vue instance/app.js
+++ Deep dive the vue instance/app.js
@@ -8,37 +8,62 @@ var vm = new Vue({
                 firstName: 'bo',
                 lastName: 'Sorkar',
             }
-        }
+        },
+    },
+
+    methods: {
+        clickButton: function () {
+            console.log(this.$refs);
+            //this.$refs.myButton.innerText = this.message;
+        },
     },
 });
+console.log(vm.$refs);
 
-// vm.message = 'Hello world';
-// alert(vm.$el.textContent);
+{
+    /* vm.message = 'Hello world';
+     alert(vm.$el.textContent);
 
-// Vue.nextTick(function(){
-//     alert(vm.$el.textContent);
-// });
+     Vue.nextTick(function(){
+         alert(vm.$el.textContent);
+     }); */
+}
 
-// vm.$watch('counter', function(newValue, oldValue){
-//     alert('Counter changed from ' + oldValue + ' to ' + newValue + '!');
-// })
+{
+    /* vm.$watch('counter', function(newValue, oldValue){
+         alert('Counter changed from ' + oldValue + ' to ' + newValue + '!');
+     })
 
-// vm.$watch('person.name.firstName', function(newValue, oldValue){
-//     alert('First name changed from ' + oldValue + ' to ' + newValue + '!')
-// });
+     vm.$watch('person.name.firstName', function(newValue, oldValue){
+         alert('First name changed from ' + oldValue + ' to ' + newValue + '!')
+     });
 
-// vm.$watch('person.name', function(newValue, oldValue){
-//     alert('First name changed from ' + oldValue.firstName + ' to ' + newValue.firstName + '!')
-// }, {deep: true});
+     vm.$watch('person.name', function(newValue, oldValue){
+         alert('First name changed from ' + oldValue.firstName + ' to ' + newValue.firstName + '!')
+     }, {deep: true}); */
+}
 
-var unwatch = vm.$watch(
-    function(){
-        return this.counter;
-    }, 
-    function(newValue, oldValue){
-        alert('Counter changed from ' + oldValue + ' to ' + newValue + '!');
-});
+{
+    /* adding watch dynamically
+     var unwatch = vm.$watch(
+         function(){
+             return this.counter;
+         }, 
+         function(newValue, oldValue){
+             alert('Counter changed from ' + oldValue + ' to ' + newValue + '!');
+     }); */
+}
 
-setTimeout(function(){
-    unwatch();
-}, 5000);
+{
+    /* setTimeout(function(){
+         unwatch();
+     }, 5000); */
+}
+
+
+    //accessing the dom with refs
+    setTimeout(function(){
+        vm.$refs.message.innerText = "This is a test";
+    }, 2000);
+
+    console.log(vm.$refs);
