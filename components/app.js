1eb1d31bbeefc59eec3083b48ca33454ce09ce90
eyakub <eyakubsorkar@gmail.com>
2019-07-31 15:32:17 +0600

    data property must be function, #it can handle same componenet with different counter value,

--- components/app.js
+++ components/app.js
@@ -1,23 +1,37 @@
-Vue.component('contact-us', {
+// INTRODUCTION TO COMPONENTS
+// Vue.component('contact-us', {
+//     data: function(){
+//         return {
+//             email: 'info@mycompany.com',
+
+//         };
+//     },
+//     template: `
+//         <div>
+//             <h1>Contact Us</h1>
+//             <p>Please send an e-mail to: {{ email }} </p>
+//         </div>
+//     `
+// });
+
+
+Vue.component('counter', {
     data: function(){
         return {
-            email: 'info@mycompany.com',
-
+            counter: 0
         };
     },
-    template: `
-        <div>
-            <h1>Contact Us</h1>
-            <p>Please send an e-mail to: {{ email }} </p>
+    template:  `
+        <div> 
+            <button @click="counter++">{{counter}} </button>
         </div>
     `
 });
 
-
 var vm = new Vue({
     el: "#app",
-    data: {
-        message: 'Hello world',
-    }
+    // data: {
+    //     message: 'Hello world',
+    // }
 });
 
