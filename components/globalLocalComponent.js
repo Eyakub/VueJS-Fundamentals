25ff6d3b83d532122305f24afa58c363ecad866c
eyakub <eyakubsorkar@gmail.com>
2019-07-31 15:46:23 +0600

    vue local and global componenets example

---
+++ components/globalLocalComponent.js
@@ -0,0 +1,29 @@
+var contactUs = {
+    data: function(){
+        return {
+            email: 'info@mycompany.com',
+        };
+    },
+    template: `
+        <div>
+            <h1>Contact Us</h1>
+            <p>Please send an e-mail to: {{ email }} </p>
+        </div>
+    `
+};
+
+
+// componenet will regester for only first app, as its
+// declared first app
+new Vue({
+    el: "#app1",
+    components: {
+        'contact-us': contactUs,
+    },
+});
+
+// i didnt declare components in this 2nd app
+new Vue({
+    el: "#app2",
+    
+});
