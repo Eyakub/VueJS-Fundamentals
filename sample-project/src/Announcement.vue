de9c498741db1c68c60f1ae88a8eb1119b057e18
eyakub <eyakubsorkar@gmail.com>
2019-07-31 16:47:07 +0600

    vue creating component in the project

---
+++ sample-project/src/Announcement.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-info">
+        {{ announcement}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                announcement: 'The site will go down for maintenance soon',
+            };
+        }
+    }
+</script>
--- sample-project/src/main.js
+++ sample-project/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Announcement from './Announcement.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.component('announcement', Announcement);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
